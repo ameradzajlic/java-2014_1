c95228cc6ba3a0aefe816127a1d957c844458f1a
unknown <lamaniobra@gmail.com>
2014-10-21 00:52:17 +0300

    Debug example jcex042014

---
+++ jcex042014/DebugExample/src/debugexample/DebugExample.java
@@ -0,0 +1,11 @@
+package debugexample;
+public class DebugExample {
+    public static void main(String[] args) {
+        System.out.println("Hello...");
+        System.out.println("World...");
+        System.out.println("And...");
+        System.out.println("Hello...");
+        System.out.println("Universe...");
+    }
+    
+}
