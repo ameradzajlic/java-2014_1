b57d1a27e051cc81b4bc03aee9ab7064eb964213
unknown <lamaniobra@gmail.com>
2014-10-21 13:44:58 +0300

    renamed:    jcex042014/DebugExample/build.xml -> jcex052014/Ex1/build.xml 	new file:   jcex052014/Ex1/manifest.mf 	renamed:    jcex042014/DebugExample/nbproject/build-impl.xml -> jcex052014/Ex1/nbproject/build-impl.xml 	renamed:    jcex042014/DebugExample/nbproject/genfiles.properties -> jcex052014/Ex1/nbproject/genfiles.properties 	renamed:    jcex042014/DebugExample/nbproject/project.properties -> jcex052014/Ex1/nbproject/project.properties 	renamed:    jcex042014/DebugExample/nbproject/project.xml -> jcex052014/Ex1/nbproject/project.xml 	new file:   jcex052014/Ex1/src/ex1/Ex1.java 	new file:   jcex052014/Ex2/build.xml 	new file:   jcex052014/Ex2/manifest.mf 	new file:   jcex052014/Ex2/nbproject/build-impl.xml 	new file:   jcex052014/Ex2/nbproject/genfiles.properties 	new file:   jcex052014/Ex2/nbproject/project.properties 	new file:   jcex052014/Ex2/nbproject/project.xml 	new file:   jcex052014/Ex2/src/ex2/Ex2.java

---
+++ jcex052014/Ex1/src/ex1/Ex1.java
@@ -0,0 +1,21 @@
+package ex1; 
+public class Ex1 { 
+    public static void main(String[] args) {
+        /**
+        * first operand
+        */
+        int operand1=1;
+        /**
+        * second operand
+        */
+        int operand2=2;
+        /**
+        * summation
+        */
+        int sum=operand1+operand2;
+        /**
+        * result display
+        */
+        System.out.println(sum);
+    }
+}
---
+++ jcex052014/Ex2/src/ex2/Ex2.java
@@ -0,0 +1,14 @@
+package ex2;
+public class Ex2 {
+    public static void main(String[] args) {
+        int a=2,b=3;
+        /*
+        System.out.print("First operand: ");
+        System.out.println(a);
+        System.out.print("Second operand: ");
+        System.out.println(b);
+        System.out.print("Sum: ");
+        */
+        System.out.println(a+b);
+    } 
+}
