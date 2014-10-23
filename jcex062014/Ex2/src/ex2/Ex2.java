b59025d1d6bca6f9dc9eae034d892b6e7a0ab25d
unknown <lamaniobra@gmail.com>
2014-10-23 19:10:49 +0300

    ex1, ex2

---
+++ jcex062014/Ex2/src/ex2/Ex2.java
@@ -0,0 +1,8 @@
+package ex2;
+public class Ex2 {
+    public static void main(String[] args) {
+        double x = 15.757;  
+        int y = (int) x;
+        System.out.println(y);
+    } 
+}
---
+++ jcex062014/ex1/src/ex1/Ex1.java
@@ -0,0 +1,8 @@
+package ex1;
+public class Ex1 {
+    public static void main(String[] args) {
+        int years = 35;
+        double salary = 1500.00; 
+        System.out.println("Employee is " + years + " old, and it's salary is " + salary);
+    }
+}
